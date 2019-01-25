677f9f97befcdc05ce2b432873af9db2211b7701
Evaldas Dzimanavicius <ewaldaz@gmail.com>
2019-01-26 00:39:05 +0200

    Added multiplayer

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+    public float TransitionSpeed = 5f;
+    public Vector3 Offset;
+
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        if (!Player)
+        {
+            Player = GameObject.FindWithTag("Player")?.GetComponent<Transform>();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (Player)
+        {
+            Vector3 position = Player.position + Offset;
+            Vector3 transition = Vector3.Lerp(transform.position, position, TransitionSpeed * Time.deltaTime);
+            transform.position = transition;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PlayerController : MonoBehaviour
 {
     float x;
     float z;
+    float jump;
+    Vector3 prevLoc;
+    public float lookSpeed = 10;
+    bool running = false;
+    bool up = false;
+    float jumpVelocity = 500f;
+
     // Update is called once per frame
     void Update()
     {
@@ -15,12 +23,42 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        prevLoc = transform.position;
         transform.position = transform.position + new Vector3(x, 0, z);
+
+        #region Rotation and animations
+        if (transform.position != prevLoc || running)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position - prevLoc), Time.fixedDeltaTime * lookSpeed);
+          //  GetComponent<Animator>().Play("Running");
+        }
+        else
+        {
+           // GetComponent<Animator>().Play("Idle");
+        }
+        #endregion Rotation and animations
+
+        #region Jump
+        if (up)
+        {
+            if (GetComponent<Rigidbody>().velocity.y <= 0 && GetComponent<Rigidbody>().velocity.y > -0.01)
+            {
+                GetComponent<Rigidbody>().AddForce(0, jumpVelocity, 0);
+            }
+            up = false;
+        }
+        #endregion Jump
     }
 
     private void ReadInputs()
     {
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
+
+        jump = Input.GetAxis("Jump");
+        if (jump != 0)
+        {
+            up = true;
+        }
     }
 }
