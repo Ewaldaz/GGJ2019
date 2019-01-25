04f9891b985268c9dcf7751c9c89f2cf62527919
Evaldas Dzimanavicius <ewaldaz@gmail.com>
2019-01-25 20:44:47 +0200

    Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float x;
+    float z;
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInputs();
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(x, 0, z);
+    }
+
+    private void ReadInputs()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+    }
+}
